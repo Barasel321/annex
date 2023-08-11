3d83cbe4c62a20b1d00387bbc617e0ac49eeb1bb
Barasel321 <barsoumfikry@hotmail.com>
2023-08-10 20:19:33 -0400

    Initial commit

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 4f;
+
+    public float smoothTime = 0.1f;
+    
+    Vector3 velocity;
+    float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool onGround;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 hDirection = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //Horizontal Translation
+        if (Mathf.Pow(hDirection.x,2) + Mathf.Pow(hDirection.z,2) >= 0.01f)
+        {   
+            float targetAngle = Mathf.Atan2(hDirection.x, hDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(Time.deltaTime * speed * moveDirection.normalized);
+        }
+
+        //Vertical Translation aka gravity
+
+        onGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (onGround && velocity.y < 0){
+            velocity.y = -1f;
+            if (Input.GetButtonDown("Jump")){
+                //replace for no sqrt?
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        
+        
+    }
+}
